c6fa44cab26f35ad70d58f4fd37adc4eb11bc90f
Princess <pcjbautista@gmail.com>
2020-12-04 18:45:32 -0800

    added @IgnoreExtraProperties teehee

--- app/src/main/java/com/example/cc17favoriteytchannels/models/Channel.kt
+++ app/src/main/java/com/example/cc17favoriteytchannels/models/Channel.kt
@@ -1,5 +1,8 @@
 package com.example.cc17favoriteytchannels.models
 
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 class Channel (var id: String? = "", var name: String? = "", var link: String? = "", var rank: Int = 0 , var reason: String? = "") : Comparable<Channel>{
     override fun toString(): String {
         return "$rank : $name \n\t : $link \n\t : $reason"
